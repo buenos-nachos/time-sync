4cf73cb103ba3db8f1b3565862840475af238541
Michael Smith <throwawayclover@gmail.com>
2025-11-07 05:30:29 -0600

    docs: add comment to clarify why polyfill is here

--- packages/react/src/useEffectEventPolyfill.ts
+++ packages/react/src/useEffectEventPolyfill.ts
@@ -1,8 +1,9 @@
 import { useCallback, useInsertionEffect, useRef } from "react";
 
 /**
- * Implemented to enable support for React 18. useEffectEvent was only added
- * to the core library in verison 19.2.
+ * Implemented to enable support for React on versions 18 (released March
+ * 29, 2022) up to version 19.1.1. useEffectEvent was only added to the
+ * core library in verison 19.2 (October 1, 2025).
  */
 export function useEffectEvent<TArgs extends unknown[], TReturn = unknown>(
 	callback: (...args: TArgs) => TReturn,
