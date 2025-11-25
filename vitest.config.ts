ac6bf2210739572ae9469745ab76bf67491b7efb
Michael Smith <throwawayclover@gmail.com>
2025-11-25 12:26:14 -0500

    docs: add comment to vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,6 +5,10 @@ export default defineConfig({
 		include: ["**/*.test.{ts,tsx}"],
 		environment: "happy-dom",
 		env: {
+			// This is necessary to prevent flakes when running some tests in
+			// GitHub Actions. Date objects don't give you an option to
+			// configure the device time zone, so you have to do it at the
+			// environment level
 			TZ: "US/Eastern",
 		},
 	},
