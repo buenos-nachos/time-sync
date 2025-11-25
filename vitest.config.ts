37ab2d99db6b9d36c98db85dfa74cf48aa292a98
Michael Smith <throwawayclover@gmail.com>
2025-11-25 12:49:24 -0500

    chore: prepare repo for publishing (#21)
    ## Changes made
    - Updated CI scripts to make flows a little easier and reliable
    - Updated all actions to use frozen lockfiles when installing PNPM
    dependencies
       - Added step for verifying builds
       - Split typo check into separate action
    - Updated all `package.json` files
    - Updated root file to make sure it's configured properly for workspaces
    and can work with the changesets CLI tool
       - Updated `time-sync` file to make entry points more explicit
    - Added `tsdown` (and necessary files) to project to simplify build
    process for all packages going forward
    - Turned off TypeScript compilation through TSC (will be handled
    entirely through tsdown going forward)
    - Added Biome overrides to account for `tsdown.config.ts` and
    `vitest.config.ts` files
    - Added note about why `vitest.config.ts` file needs timezone override
    
    ## Notes
    - The structure of the project is becoming specific enough that I feel
    like I'll want to adds some scripts (in Go/Bash/TypeScript) for
    validating the structure of the repo, and then add that check to CI

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from "vitest/config";
 
-// biome-ignore lint:style/noDefaultExport -- Vite expects this export type
 export default defineConfig({
 	test: {
 		include: ["**/*.test.{ts,tsx}"],
 		environment: "happy-dom",
 		env: {
+			// This is necessary to prevent flakes when running some tests in
+			// GitHub Actions. Date objects don't give you an option to
+			// configure the device time zone, so you have to do it at the
+			// environment level
 			TZ: "US/Eastern",
 		},
 	},
