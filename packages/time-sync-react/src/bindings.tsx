b39cfcaaecb1d8c25a204d8f653841acb8880897
Michael Smith <throwawayclover@gmail.com>
2025-12-11 12:55:25 -0500

    chore: update init logic

--- packages/time-sync-react/src/bindings.tsx
+++ packages/time-sync-react/src/bindings.tsx
@@ -2,7 +2,7 @@
  * @file When updating the comments for the useTimeSync and useTimeSyncRef
  * hooks, be sure to copy them to hooks.ts as well.
  */
-import type { ReadonlyDate, TimeSync } from "@buenos-nachos/time-sync";
+import type { TimeSync, ReadonlyDate } from "@buenos-nachos/time-sync";
 import {
 	type Context,
 	createContext,
@@ -86,45 +86,55 @@ type CreateReactBindingsResult<
 	TInject extends InjectionMethod,
 > = TInject extends "closure"
 	? {
+			readonly cleanup: () => void;
 			readonly useTimeSyncRef: UseTimeSyncRef;
 			readonly useTimeSync: <TData = ReadonlyDate>(
 				options: UseTimeSyncOptions<TData>,
 			) => UseTimeSyncResult<TIsServerRendered, TData>;
 		}
-	: {
-			/**
-			 * Sets up a new TimeSync subscription using the specified
-			 * interval, and ensures that the component will be able to
-			 * re-render as the TimeSync instance updates its internal state
-			 * and notifies subscribers.
-			 *
-			 * The returned value is fully bound to React's lifecycles, and is
-			 * always safe to reference inside render logic, event handlers, and
-			 * effects.
-			 *
-			 * See the `UseTimeSyncOptions` type for more info on what each
-			 * property does.
-			 */
-			readonly useTimeSync: <TData = ReadonlyDate>(
-				options: UseTimeSyncOptions<TData>,
-			) => UseTimeSyncResult<TIsServerRendered, TData>;
+	: TInject extends "hybrid"
+		? {
+				readonly cleanup: () => void;
+				readonly TimeSyncProvider: TimeSyncProvider;
+				readonly useTimeSyncRef: UseTimeSyncRef;
+				readonly useTimeSync: <TData = ReadonlyDate>(
+					options: UseTimeSyncOptions<TData>,
+				) => UseTimeSyncResult<TIsServerRendered, TData>;
+			}
+		: {
+				/**
+				 * Sets up a new TimeSync subscription using the specified
+				 * interval, and ensures that the component will be able to
+				 * re-render as the TimeSync instance updates its internal state
+				 * and notifies subscribers.
+				 *
+				 * The returned value is fully bound to React's lifecycles, and is
+				 * always safe to reference inside render logic, event handlers, and
+				 * effects.
+				 *
+				 * See the `UseTimeSyncOptions` type for more info on what each
+				 * property does.
+				 */
+				readonly useTimeSync: <TData = ReadonlyDate>(
+					options: UseTimeSyncOptions<TData>,
+				) => UseTimeSyncResult<TIsServerRendered, TData>;
 
-			/**
-			 * Exposes the raw TimeSync instance without binding it to React
-			 * state. The TimeSync itself is safe to pass around inside a
-			 * render, but ALL of its methods must be called from inside event
-			 * handlers or useEffect calls.
-			 *
-			 * This hook is mainly intended as an escape hatch for when
-			 * `useTimeSync` won't serve your needs.
-			 */
-			readonly useTimeSyncRef: UseTimeSyncRef;
+				/**
+				 * Exposes the raw TimeSync instance without binding it to React
+				 * state. The TimeSync itself is safe to pass around inside a
+				 * render, but ALL of its methods must be called from inside event
+				 * handlers or useEffect calls.
+				 *
+				 * This hook is mainly intended as an escape hatch for when
+				 * `useTimeSync` won't serve your needs.
+				 */
+				readonly useTimeSyncRef: UseTimeSyncRef;
 
-			/**
-			 * @todo Need to figure out the best way to describe this
-			 */
-			readonly TimeSyncProvider: TimeSyncProvider;
-		};
+				/**
+				 * @todo Need to figure out the best way to describe this
+				 */
+				readonly TimeSyncProvider: TimeSyncProvider;
+			};
 
 // Making flattened, "non-clever" version of the above types so that they're
 // easier to work with internally. The main problem with them is that they don't
@@ -137,8 +147,10 @@ interface FlatCreateReactBindingsOptions {
 interface FlatCreateReactBindingsResult {
 	readonly useTimeSync: unknown;
 	readonly useTimeSyncRef: UseTimeSyncRef;
-	// Left mutable on purpose; it'll become readonly before it reaches users
+
+	// Left mutable on purpose; they'll become readonly before it reaches users
 	TimeSyncProvider?: TimeSyncProvider;
+	cleanup?: () => void;
 }
 
 function validateCreateReactBindingsOptions(
@@ -178,18 +190,29 @@ export function createReactBindings<
 	// to get more complicated and nuanced over time. Code duplication is better
 	// than bad abstractions right now
 	let TimeSyncProvider: TimeSyncProvider | undefined;
+	let cleanup: (() => void) | undefined;
 	let get: ReactTimeSyncGetter;
 	switch (injectionMethod) {
 		case "closure": {
 			const fixedRts = new ReactTimeSync(timeSync);
 			get = () => fixedRts;
 			TimeSyncProvider = undefined;
-			fixedRts.onProviderMount();
+
+			const cleanupInit = fixedRts.onAppInit("default-init-closure");
+			const cleanupMount = fixedRts.onProviderMount();
+			cleanup = () => {
+				// Have to make sure we clean up the mount first, or we'll get an error
+				cleanupMount();
+				cleanupInit();
+			};
 			break;
 		}
 
 		case "reactContext": {
 			const context = createContext<ReactTimeSync | undefined>(undefined);
+			TimeSyncProvider = createTimeSyncProvider(context);
+			cleanup = undefined;
+
 			get = function useReactTimeSyncContext() {
 				const value = useContext(context);
 				if (value === undefined) {
@@ -199,7 +222,6 @@ export function createReactBindings<
 				}
 				return value;
 			};
-			TimeSyncProvider = createTimeSyncProvider(context);
 			break;
 		}
 
@@ -210,11 +232,17 @@ export function createReactBindings<
 			// there's a meaningful default value
 			const defaultRts = new ReactTimeSync(timeSync);
 			const context = createContext(defaultRts);
+			TimeSyncProvider = createTimeSyncProvider(context);
 			get = function useReactTimeSyncContextWithDefault() {
 				return useContext(context);
 			};
-			TimeSyncProvider = createTimeSyncProvider(context);
-			defaultRts.onProviderMount();
+
+			const cleanupInit = defaultRts.onAppInit("default-init-hybrid");
+			const cleanupMount = defaultRts.onComponentMount();
+			cleanup = () => {
+				cleanupMount();
+				cleanupInit();
+			};
 			break;
 		}
 
@@ -230,9 +258,12 @@ export function createReactBindings<
 		useTimeSync: createUseTimeSync<TIsServerRendered>(get),
 		useTimeSyncRef: createUseTimeSyncRef(get),
 	};
-	// Only add the key at runtime if we actually have a meaningful value
+	// Only add the keys at runtime if we actually have meaningful values
 	if (TimeSyncProvider !== undefined) {
 		result.TimeSyncProvider = TimeSyncProvider;
+	}
+	if (cleanup !== undefined) {
+		result.cleanup = cleanup;
 	}
 	return result as CreateReactBindingsResult<TIsServerRendered, TInject>;
 }
